fe9e0d03c0d259964febb5a935c920799a97f62d
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2019-06-18 17:17:51 +0100

    Add basic template generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+//! ``chromatic``
+//!
+//! A toolkit to assist color scheme design in Vim.
+//! Uses a TOML config to generate a vim color scheme.
+
+use askama::Template;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod scheme;
+use scheme::*;
+
+/// Command Line Input
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct ChromaticArguments {
+    /// Chromatic Config File
+    #[structopt(name = "INPUT")]
+    input: PathBuf,
+
+    /// Output File
+    #[structopt(short = "o", long = "output")]
+    output: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Fetch arguments
+    let args = ChromaticArguments::from_args();
+
+    // Check Config
+    if args.input.exists() {
+        // Parse Config
+        let config = ChromaticConfig::try_from(args.input)?;
+
+        // Parse Template from Config
+        let template = ChromaticTemplate::try_from(config)?;
+
+        // Render File to Output
+        let mut output = File::create(args.output)?;
+        output.write_all(template.render()?.as_bytes())?;
+    } else {
+        println!("Cannot find input file: {:?}", args.input.as_os_str());
+    }
+
+    Ok(())
 }
---
+++ src/scheme.rs
@@ -0,0 +1,139 @@
+//! The main color scheme template.
+//!
+//! Uses ``askama`` to generate a vim template from the given TOML file.
+
+use askama::Template;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+/// Main Color Scheme Template
+#[derive(Template)]
+#[template(path = "colorscheme")]
+pub struct ChromaticTemplate {
+    /// Information Section
+    pub information: Information,
+
+    /// Highlights Section
+    pub highlights: HashMap<String, Highlight>,
+}
+
+impl TryFrom<ChromaticConfig> for ChromaticTemplate {
+    type Error = Box<Error>;
+
+    fn try_from(config: ChromaticConfig) -> Result<Self, Self::Error> {
+        // Match the colors into highlights
+        let mut matched_highlights = HashMap::new();
+        for (n, mut h) in config.highlights {
+            println!(
+                "BG: {:?}, FG: {:?}, ST: {:?}",
+                h.background_color, h.foreground_color, h.style_color
+            );
+
+            // Match the background color
+            match h.background_color {
+                Some(x) => {
+                    let bg_color = match config.colors.get(&x) {
+                        Some(x) => x.hex.to_owned(),
+                        None => String::from("none")
+                    };
+                    println!("BG: Matched with {}", bg_color);
+                    h.background_color = Some(bg_color);
+                }
+                None => println!("BG: Matched None"),
+            }
+
+            // Match the foreground color
+            match h.foreground_color {
+                Some(x) => {
+                    let fg_color = match config.colors.get(&x) {
+                        Some(x) => x.hex.to_owned(),
+                        None => String::from("none")
+                    };
+                    println!("FG: Matched with {}", fg_color);
+                    h.foreground_color = Some(fg_color);
+                }
+                None => println!("FG: Matched None"),
+            }
+
+            // Match the style color
+            match h.style_color {
+                Some(x) => {
+                    let st_color = match config.colors.get(&x) {
+                        Some(x) => x.hex.to_owned(),
+                        None => String::from("none")
+                    };
+                    println!("ST: Matched with {}", st_color);
+                    h.style_color = Some(st_color);
+                }
+                None => println!("ST: Matched None"),
+            }
+
+            matched_highlights.insert(n, h);
+        }
+
+        Ok(Self {
+            information: config.information,
+            highlights: matched_highlights,
+        })
+    }
+}
+
+/// Main Color Scheme Config
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ChromaticConfig {
+    /// Information Section
+    pub information: Information,
+
+    /// Colors Section
+    #[serde(serialize_with = "toml::ser::tables_last")]
+    pub colors: HashMap<String, Color>,
+
+    /// Highlights Section
+    #[serde(serialize_with = "toml::ser::tables_last")]
+    pub highlights: HashMap<String, Highlight>,
+}
+
+/// Generate a config from a ``PathBuf``.
+impl TryFrom<PathBuf> for ChromaticConfig {
+    type Error = Box<Error>;
+
+    fn try_from(path: PathBuf) -> Result<Self, Self::Error> {
+        let mut file = File::open(path.as_path())?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        Ok(toml::from_str(&contents)?)
+    }
+}
+
+/// Information Structure for the configuration file.
+#[derive(Deserialize, Debug, Serialize, Clone)]
+pub struct Information {
+    pub name: String,
+    pub scheme_name: String,
+    pub author: String,
+    pub background: String,
+    pub description: Option<String>,
+    pub license: Option<String>,
+}
+
+/// Color Structure for the configuration file.
+#[derive(Deserialize, Debug, Serialize, Clone)]
+pub struct Color {
+    pub hex: String,
+    pub color_integer: u8,
+}
+
+/// Highlight Structure for the configuration file.
+#[derive(Deserialize, Debug, Serialize, Clone)]
+pub struct Highlight {
+    pub background_color: Option<String>,
+    pub foreground_color: Option<String>,
+    pub style: Option<String>,
+    pub style_color: Option<String>,
+}
